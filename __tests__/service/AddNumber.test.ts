332f23792bb6256a6c90c28477f14989f3567a0b
suyeb <shobekhan@yahoo.com>
2022-07-14 16:21:35 +0200

    main: simple test added and dev dependencies added

---
+++ __tests__/service/AddNumber.test.ts
@@ -0,0 +1,26 @@
+import AddNumber from '../../src/service/AddNumber';
+
+describe('Test function AddNumber', () => {
+
+    let result: number;
+
+    beforeEach(() => {
+        result = 0;
+    })
+
+    it('checks function returns valid numbers', () => {
+        result = AddNumber(10, 100);
+        expect(result).toBe(110);
+
+        result = AddNumber(100, 100);
+        expect(result).toBe(200);
+    });
+
+    it('checks function does not returns in valid numbers', () => {
+        result = AddNumber(10, 100);
+        expect(result).not.toBe(1110);
+
+        result = AddNumber(100, 100);
+        expect(result).not.toBe(2000);
+    });
+});
---
+++ src/service/AddNumber.ts
@@ -0,0 +1,5 @@
+let AddNumber = (x: number, y: number): number => {
+    return x + y;
+}
+
+export default AddNumber;
